a8129b1fa01fd35dae4d547ee17e0601317608f1
andrbor <andrbor02@yandex.ru>
2020-10-24 15:12:25 +0300

    Giri

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        System.out.println("Результат сложения: " + (a + b));
+        int N = in.nextInt();
+        int c = 0;
+        do{
+            N /= 2;
+            c++;
+        }while(N >= 1);
+        System.out.println(c);
     }
 }
