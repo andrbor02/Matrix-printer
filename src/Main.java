f7f4086dc65be5f63069f9dd80ff5d0d56a5489c
andrbor <andrbor02@yandex.ru>
2020-10-25 00:46:13 +0300

    1.2.4

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
-        int b;
-        b = a - (a%2) + 2;
-        System.out.println(b);
+        int b = in.nextInt();
+        int n = in.nextInt();
+        int ans;
+        ans = ((a * 100 + b) * n);
+        System.out.println((ans - ans % 100) / 100 + " " +ans % 100);
     }
 }
