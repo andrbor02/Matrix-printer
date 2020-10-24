95c1c2e6dd624372bfb96c043576b76999adae68
andrbor <andrbor02@yandex.ru>
2020-10-25 00:31:49 +0300

    1.2.3

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
-        int b, c ,d;
-        b = Math.abs(a % 10);
-        c = Math.abs((a % 100 - b)/10);
-        d = Math.abs((a % 1000 - c)/100);
-        System.out.println(b + c + d);
+        int b;
+        b = a - (a%2) + 2;
+        System.out.println(b);
     }
 }
