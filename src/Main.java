8b4ab2a666321201bb92b67e8aa2ef41c133f589
andrbor <andrbor02@yandex.ru>
2020-10-24 23:39:00 +0300

    1.2.1

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int c = 0;
-        do{
-            N /= 2;
-            c++;
-        }while(N > 1);
-        System.out.println(c+4);
-        System.out.println(c+3);
-        System.out.println(c+2);
-        System.out.println(c+1);
-        System.out.println(c);
+        int a = in.nextInt();
+        int b;
+        b = a % 10;
+        System.out.println(Math.abs(b));
     }
 }
