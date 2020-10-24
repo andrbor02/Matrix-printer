524f1690d8971c4e5ef2815417af541a3a824945
andrbor <andrbor02@yandex.ru>
2020-10-24 22:15:23 +0300

    test prog

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,10 @@ public class Main {
             N /= 2;
             c++;
         }while(N > 1);
+        System.out.println(c+4);
+        System.out.println(c+3);
+        System.out.println(c+2);
+        System.out.println(c+1);
         System.out.println(c);
     }
 }
