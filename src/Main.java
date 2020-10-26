5973e309c7ce13d66ebd5f7015e6838efc25aadc
andrbor <andrbor02@yandex.ru>
2020-10-26 13:00:02 +0300

    Addit. 1.2.1

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-        int numb = 0;
-        int ans = 9;
-        int i = 0;
-        int c4et = (int)Math.log10(N)+1;
-        while(c4et > 0) {
-            numb = (int) ((N % Math.pow(10, i + 1) - numb) / Math.pow(10, i));
-            if(numb < ans)
-                ans = numb;
-            i++;
-            c4et--;
-        }
-        System.out.println(ans);
+        System.out.println(Math.abs(N%10));
     }
 }
