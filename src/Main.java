9ef879d96d3acfa22beb02f9afe1bd48ab83cd70
andrbor <andrbor02@yandex.ru>
2020-10-26 16:42:09 +0300

    1.4.1 not final

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int A = in.nextInt();
-        int B = in.nextInt();
-        System.out.println(A * A + B * B);
+        int i = in.nextInt();
+        int Ans;
+        int doubled_Ans;
+        int doubled_A = 0;
+        int reversed_doubled_A = 0;
+
+        /*int t = 0;
+        while(A >= 1){
+            doubled_A = doubled_A + ((A % 2) * (int)(Math.pow(10, t)));
+            A = A / 2;
+            t++;
+        }
+        System.out.println(doubled_A);
+        while(doubled_A > 0) {
+            reversed_doubled_A = reversed_doubled_A * 10 + doubled_A % 10;
+            doubled_A /= 10;
+        }*/
+        doubled_A = Integer.parseInt(Integer.toBinaryString(A));
+        doubled_Ans = doubled_A - (int)(doubled_A % Math.pow(10, i));
+        Ans = Integer.parseInt(String.valueOf(doubled_Ans), 2);
+
+        System.out.println(Ans);
     }
 }
