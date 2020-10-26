b81e3068ae2eac122785ac192372ebcca19dc1fe
andrbor <andrbor02@yandex.ru>
2020-10-26 20:46:50 +0300

    1.4.2

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int A = in.nextInt();
-        int i = in.nextInt();
-        long Ans;
-        long doubled_Ans;
-        //doubled doubled_A = 0;
+        int n = in.nextInt();
 
-        A = A >>> i; // сдвигаем на i бит  вправо 110001 на 2 -> 110    отличие >> от >>> https://fooobar.com/questions/26011/difference-between-and
-        A = A << i; // сдвигаем на i бит влево 110 на 2 -> 11000
+        n = 1 << n;
 
-        System.out.println(A);
+        System.out.println(n);
 
-        /*int t = 0;
-        while(A >= 1){
-            doubled_A = doubled_A + ((A % 2) * (int)(Math.pow(10, t)));
-            A = A / 2;
-            t++;
-        }
-        System.out.println(doubled_A);*/
-
-
-       // String s = Integer.toBinaryString(A);
-       // System.out.println(s);
-        //doubled_A = Long.parseLong(s);
-       // doubled_Ans = doubled_A - (long)(doubled_A % Math.pow(10, i));
-       // Ans = Long.parseLong(String.valueOf(doubled_Ans), 2);
-
-       // System.out.println(Ans);
     }
 }
