2712f09e812ebdda0a7656d4626df54380f00755
andrbor <andrbor02@yandex.ru>
2020-10-26 13:03:41 +0300

    Addit. 1.2.2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-        System.out.println(Math.abs(N%10));
+        System.out.println((N - N % 10) / 10);
     }
 }
