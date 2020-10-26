c9b31569e3144c70293ca22b30ecf0feef93c14d
andrbor <andrbor02@yandex.ru>
2020-10-26 12:57:13 +0300

    Least numeral in number

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int h;
-        int m;
-        int s;
-
-        h=(n/3600)%24;
-        m=(n%3600)/60;
-        s=(n%3600)%60;
-
-        System.out.printf("%d:%02d:%02d", h, m, s);
+        int N = in.nextInt();
+        int numb = 0;
+        int ans = 9;
+        int i = 0;
+        int c4et = (int)Math.log10(N)+1;
+        while(c4et > 0) {
+            numb = (int) ((N % Math.pow(10, i + 1) - numb) / Math.pow(10, i));
+            if(numb < ans)
+                ans = numb;
+            i++;
+            c4et--;
+        }
+        System.out.println(ans);
     }
 }
