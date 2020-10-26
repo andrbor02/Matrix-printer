bc7e31229ba0b42a007a1236db4ce447dd5ea19c
andrbor <andrbor02@yandex.ru>
2020-10-26 13:04:04 +0300

    Addit. 1.2.3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        System.out.println((N - N % 10) / 10);
+        int a = in.nextInt();
+        int b, c ,d;
+        b = Math.abs(a % 10);
+        c = Math.abs((a % 100 - b)/10);
+        d = Math.abs((a % 1000 - c)/100);
+        System.out.println(b + c + d);
     }
 }
