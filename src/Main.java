93d6ff77a4656ee063ff06f3d325f8485b423fd9
andrbor <andrbor02@yandex.ru>
2020-10-27 13:13:23 +0300

    1.4.6

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int x = in.nextInt();
-        int i = in.nextInt();
-        int N = x;
+        int N = in.nextInt();
+        int copy_N = N;
         int t = 0;
         do{
-            N /= 2;
+            copy_N /= 2;
             t++;
-        }while(N >= 1);
-        while (t > i) {
-            x &= ~(1 << (t - 1)); // сбросить бит 
-            t--;
-        }
-        System.out.println(x);
+        }while(copy_N >= 1);
+
+        if(N == (1 << (t - 1)))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
     }
 }
