b70fcc07d2684745566fc9964005f49e84a3a9c7
andrbor <andrbor02@yandex.ru>
2020-10-27 12:54:56 +0300

    1.4.5

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int x = in.nextInt();
-        int n = in.nextInt();
-
-        x = x ^ (1 << n);
-
+        int i = in.nextInt();
+        int N = x;
+        int t = 0;
+        do{
+            N /= 2;
+            t++;
+        }while(N >= 1);
+        while (t > i) {
+            x &= ~(1 << (t - 1)); // сбросить бит 
+            t--;
+        }
         System.out.println(x);
     }
 }
