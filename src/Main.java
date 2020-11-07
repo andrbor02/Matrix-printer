a5c0c8e60e98aff00b2280f49c3811ddc75c0ddc
andrbor <andrbor02@yandex.ru>
2020-11-07 14:28:58 +0300

    Bridges

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int copy_N = N;
-        int t = 0;
-        do{
-            copy_N /= 2;
-            t++;
-        }while(copy_N >= 1);
+        int number_of_bridges = in.nextInt();
 
-        if(N == (1 << (t - 1)))
-            System.out.println("YES");
-        else
-            System.out.println("NO");
+        int i = 1;
+        while(i < N){
+            t = in.nextInt();
+            if (t <= 437){
+                out.println("Crash " + i);
+                break;
+            }
+            i++;
+        }
+        if (t > 437){
+            out.println("No сrash");
+        }
     }
 }
