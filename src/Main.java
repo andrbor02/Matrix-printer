25b91bdab739c413217f5ffa502c56769ace1dff
andrbor <andrbor02@yandex.ru>
2020-11-08 19:07:13 +0300

    1.5.5

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         in.useLocale(US);
-        double x = in.nextDouble();
-        boolean e = false;
-        int r = (int)(Math.log10(x) + 1);
-        if (x % 5 == 0 && r == 3)
-            e = true;
-        System.out.println(e);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+        boolean flag = false;
+
+        if (a + b == 0 || a + c == 0 || a + d == 0 || b + c == 0 || b + d == 0 || c + d == 0)
+            flag = true;
+
+        System.out.println(flag);
     }
 }
