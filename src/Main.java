7be2fc47535aa2bf7e5e38005ab46d435c741ac8
andrbor <andrbor02@yandex.ru>
2020-11-08 20:50:29 +0300

    1.6.2

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         in.useLocale(US);
-        int x = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
 
-        if (x >= 0 && (int)Math.log10(x) == 0 || x == 0)
-            System.out.println("DIGIT");
-        else if (x >= 0 && (int)Math.log10(x) == 1)
-            System.out.println("NUM");
-        else
-            System.out.println("OTHER");
+        if (a > b && b > c)
+            System.out.println(c + " " + b + " " + a);
+        if (a > c && c > b)
+            System.out.println(b + " " + c + " " + a);
+        if (b > a && a > c)
+            System.out.println(c + " " + a + " " + b);
+        if (b > c && c > a)
+            System.out.println(a + " " + c + " " + b);
+        if (c > a && a > b)
+            System.out.println(b + " " + a + " " + c);
+        if (c > b && b > a)
+            System.out.println(a + " " + b + " " + c);
     }
 }
