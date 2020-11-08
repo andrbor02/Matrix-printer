ed4dbe0d23a2875deb11ecd804663bbeb21d37c5
andrbor <andrbor02@yandex.ru>
2020-11-08 23:01:41 +0300

    1.6.3

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,26 @@
 import java.util.*;
 
-import static java.util.Locale.US;
-
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        in.useLocale(US);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int c = in.nextInt();
+        int N = in.nextInt();
+        int copy_N = N;
+        if((N - (N % 10)) / 10 % 10 != 1)
+            copy_N = N % 10;
+        else
+            copy_N = N % 100;
 
-        if (a > b && b > c)
-            System.out.println(c + " " + b + " " + a);
-        if (a > c && c > b)
-            System.out.println(b + " " + c + " " + a);
-        if (b > a && a > c)
-            System.out.println(c + " " + a + " " + b);
-        if (b > c && c > a)
-            System.out.println(a + " " + c + " " + b);
-        if (c > a && a > b)
-            System.out.println(b + " " + a + " " + c);
-        if (c > b && b > a)
-            System.out.println(a + " " + b + " " + c);
+        switch (copy_N){
+            case 1:
+                System.out.println(N + " TOPT");
+                break;
+            case 2:
+            case 3:
+            case 4:
+                System.out.println(N + " TOPTA");
+                break;
+            default:
+                System.out.println(N + " TOPTOB");
+        }
     }
 }
