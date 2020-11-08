7f48b59c6bf1d661772c8a51caa39871d76acc1e
andrbor <andrbor02@yandex.ru>
2020-11-08 11:19:17 +0300

    1.5.1

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,15 @@
 import java.util.*;
+
+import static java.util.Locale.US;
+
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int[] elem = new int[10000];
-        int b = 0;
-
-        for(int j = 0; j < N; j++)
-            elem[j] = in.nextInt();
-
-        for(int i = 0; i < N; i++) {
-            if(elem[i] != 0) {
-                System.out.print(elem[i] + " ");
-                b++;
-            }
-        }
-        for(int k = 0; k < (N - b); k++)
-            System.out.print(0 + " ");
+        in.useLocale(US);
+        double x = in.nextDouble();
+        boolean e = false;
+        if (x >= 3 && x <= 8)
+            e = true;
+        System.out.println(e);
     }
 }
-
-// 12
