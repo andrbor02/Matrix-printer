a82a7a6b22aadd87e5a6b7534a2405e25952e241
andrbor <andrbor02@yandex.ru>
2020-11-09 00:03:25 +0300

    1.6.4

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,22 @@
+import java.text.DecimalFormat;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int copy_N = N;
-        if((N - (N % 10)) / 10 % 10 != 1)
-            copy_N = N % 10;
-        else
-            copy_N = N % 100;
+        double A = in.nextDouble();
+        double B = in.nextDouble();
 
-        switch (copy_N){
-            case 1:
-                System.out.println(N + " TOPT");
-                break;
-            case 2:
-            case 3:
-            case 4:
-                System.out.println(N + " TOPTA");
-                break;
-            default:
-                System.out.println(N + " TOPTOB");
-        }
+        double bap = B / A;
+        double bam = -B / A;
+
+        if(A <= 0 && B <= 0)
+            System.out.println("no such x");
+        else if (A >= 0 && B > 0 || A > 0 && B == 0)
+            System.out.println("any x");
+        else if (A > 0 && B < 0)
+            System.out.printf("x<%.1f or x>%.1f", bap, bam);
+        else
+            System.out.printf("%.1f<x<%.1f", bap, bam);
     }
 }
