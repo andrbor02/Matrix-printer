4978f77ea63c35ae44d943bd97d551bdc1433597
andrbor <andrbor02@yandex.ru>
2020-11-12 22:29:47 +0300

    add 1.6.2

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,31 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        if(a > 12 || a <= 0)
-            System.out.println("0");
+        int month = in.nextInt();
+        int day = in.nextInt();
+        int past = 0;
+
+        if (month > 12 || month < 1 || day > 31 || day < 1 || (month == 2 && day > 28) || ((month == 4 || month == 6 || month == 9 || month == 11) & day > 30))
+            System.out.println("-1");
         else{
-        switch (a) {
-            case 2:
-                System.out.println("28");
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                System.out.println("30");
-                break;
-            default:
-                System.out.println("31");
-        }
+            for (int i = 1; i < month; i++) {
+                switch (i) {
+                    case 2:
+                        past += 28;
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        past += 30;
+                        break;
+                    default:
+                        past += 31;
+                        break;
+                }
+            }
+            past += day;
+            System.out.println(365 - past);
         }
     }
 }
