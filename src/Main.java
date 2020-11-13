7bcc9960c6b898e0cbdf813b1dbaed5307520266
andrbor <andrbor02@yandex.ru>
2020-11-14 02:11:02 +0300

    add 1.7.6

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int A = in.nextInt();
-        int i = 0, numb = 0, sum = 0;
-        while(i < Math.log10(A)){
-            numb = (int) ((A % (int)(Math.pow(10, (i + 1))) - numb) / Math.pow(10, i));
-            sum += numb;
+        int i = 0, numb1 = 0, numb2 = 0;
+        boolean flag = false;
+
+        while(i < Math.log10(A) - 1){
+            numb1 = (int) ((A % (int)(Math.pow(10, (i + 1))) - numb1) / Math.pow(10, i));
+            numb2 = (int) ((A % (int)(Math.pow(10, (i + 2))) - numb2) / Math.pow(10, i + 1));
+            if(numb1 == numb2)
+                flag = true;
             i++;
         }
-        System.out.println(sum);
+        if(flag)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
     }
 }
