325235c7035b54c4879c74a26278b5477cc9e76d
andrbor <andrbor02@yandex.ru>
2020-11-13 15:04:51 +0300

    add 1.6.5

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,18 @@ public class Main {
         int a = in.nextInt();
         int b = in.nextInt();
         int c = in.nextInt();
-        int Max = a;
 
-        if(b > Max)
-            Max = b;
-        if(c > Max)
-            Max = c;
-
-            System.out.print(Max);
+        if (a <= b && b <= c)
+            System.out.printf("%d %d %d", a, b, c);
+        else if (a <= c && c <= b)
+            System.out.printf("%d %d %d", a, c, b);
+        else if (b <= c && c <= a)
+            System.out.printf("%d %d %d", b, c, a);
+        else if (b <= a && a <= c)
+            System.out.printf("%d %d %d", b, a, c);
+        else if (c <= a && a <= b)
+            System.out.printf("%d %d %d", c, a, b);
+        else if (c <= b && b <= a)
+            System.out.printf("%d %d %d", c, b, a);
     }
 }
