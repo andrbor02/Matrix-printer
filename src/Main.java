bf32049c8243b7c637746f1e07587eb5b96881e6
andrbor <andrbor02@yandex.ru>
2020-11-14 01:54:42 +0300

    add 1.7.3

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int n1, n2, i = 0, ans = 0;
+        int N = in.nextInt();
+        int i = 0, t = 2;
 
-        if(a > 0 && b > 0 || a < 0 && b < 0){
-            n1 = Math.abs(a);
-            n2 = Math.abs(b);
-
-        }
-        else {
-            if (a > b){
-                n1 = a;
-                n2 = b;
-            }
-            else{
-                n1 = b;
-                n2 = a;
-            }
-
-        }
-        while(i < n1){
-            ans += n2;
+        while(i < N){
+            System.out.print(t + " ");
             i++;
+            t+= 2;
         }
-        System.out.println(ans);
     }
 }
