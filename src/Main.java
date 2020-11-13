cc110c5a5faa55292f952fe531242c68418d36b6
andrbor <andrbor02@yandex.ru>
2020-11-14 00:36:50 +0300

    1.7.8

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int A = in.nextInt();
-        int i = 0;
-        while(A / 10 != 0){
-            A /= 10;
+        int i = 0, numb = 0, flag = 1;
+        while(i < Math.log10(A) + 1){
+            numb = (int) ((A % (int)(Math.pow(10, (i + 1))) - numb) / Math.pow(10, i));
+            if(numb % 2 == 1) {
+                System.out.println(numb);
+                flag = 0;
+                break;
+            }
             i++;
         }
-        System.out.println(i + 1);
+        if(flag == 1)
+        System.out.println("NO");
     }
 }
