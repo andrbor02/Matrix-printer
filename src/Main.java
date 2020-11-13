4e1d763f89a1dcc5bd4e4795068a718b1b7d48e2
andrbor <andrbor02@yandex.ru>
2020-11-14 01:51:21 +0300

    add 1.7.2

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,30 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int number_of_bridges = in.nextInt();
-        int t = 0, i = 1;
-        
-        while(i <= number_of_bridges){
-            t = in.nextInt();
-            if (t <= 437){
-                System.out.println("Crash " + i);
-                break;
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int n1, n2, i = 0, ans = 0;
+
+        if(a > 0 && b > 0 || a < 0 && b < 0){
+            n1 = Math.abs(a);
+            n2 = Math.abs(b);
+
+        }
+        else {
+            if (a > b){
+                n1 = a;
+                n2 = b;
             }
+            else{
+                n1 = b;
+                n2 = a;
+            }
+
+        }
+        while(i < n1){
+            ans += n2;
             i++;
         }
-        if (t > 437){
-            System.out.println("No crash");
-        }
+        System.out.println(ans);
     }
 }
