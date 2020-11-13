e8a20040a053b48982180cf92fd9cb44521f2bd1
andrbor <andrbor02@yandex.ru>
2020-11-14 02:15:20 +0300

    add 1.7.7

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int A = in.nextInt();
-        int i = 0, numb1 = 0, numb2 = 0;
-        boolean flag = false;
+        int i = 2;
+        boolean flag = true;
 
-        while(i < Math.log10(A) - 1){
-            numb1 = (int) ((A % (int)(Math.pow(10, (i + 1))) - numb1) / Math.pow(10, i));
-            numb2 = (int) ((A % (int)(Math.pow(10, (i + 2))) - numb2) / Math.pow(10, i + 1));
-            if(numb1 == numb2)
-                flag = true;
+        while (i < A && flag) {
+            if ((A % i) == 0) {
+                flag = false;
+                continue;
+            }
             i++;
         }
         if(flag)
-            System.out.println("YES");
+            System.out.println("prime");
         else
-            System.out.println("NO");
+            System.out.println("composite");
     }
 }
