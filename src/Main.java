3b8b597985eabf786b35f6a462636c9198282d9d
andrbor <andrbor02@yandex.ru>
2020-11-13 23:57:10 +0300

    add 1.6.6 88%

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,30 @@ public class Main {
         int a = in.nextInt();
         int b = in.nextInt();
         int c = in.nextInt();
+        int Max = a;
+        int n1 = b, n2 = c;
 
-        if (a <= b && b <= c)
-            System.out.printf("%d %d %d", a, b, c);
-        else if (a <= c && c <= b)
-            System.out.printf("%d %d %d", a, c, b);
-        else if (b <= c && c <= a)
-            System.out.printf("%d %d %d", b, c, a);
-        else if (b <= a && a <= c)
-            System.out.printf("%d %d %d", b, a, c);
-        else if (c <= a && a <= b)
-            System.out.printf("%d %d %d", c, a, b);
-        else if (c <= b && b <= a)
-            System.out.printf("%d %d %d", c, b, a);
+        if(a > b + c || b > a + c || c > a + b || a <= 0 || b <= 0 || c <= 0)
+            System.out.println("impossible");
+        else{
+
+            if(b > Max) {
+                Max = b;
+                n1 = a;
+                n2 = c;
+            }
+            if(c > Max) {
+                Max = c;
+                n1 = a;
+                n2 = b;
+            }
+
+            if(Max * Max == n1 * n1 + n2 * n2)
+                System.out.println("right");
+            if(Max * Max < n1 * n1 + n2 * n2)
+                System.out.println("acute");
+            if(Max * Max > n1 * n1 + n2 * n2)
+                System.out.println("obtuse");
+        }
     }
 }
