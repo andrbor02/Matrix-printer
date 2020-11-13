a86d8b53d2f2b74895186a82754ad41293696288
andrbor <andrbor02@yandex.ru>
2020-11-14 00:05:22 +0300

    1.7.2

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,14 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int i = 0;
+        int sum = 0;
         while(true){
             int a = in.nextInt();
-            i++;
-            if(a < 0)
+            if(a > 10)
+            sum += a;
+            if(a % 5 == 0)
                 break;
         }
-        System.out.println(i);
+        System.out.println(sum);
     }
 }
