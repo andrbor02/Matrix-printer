7266f51c946d1e087587d8c8f55f70ae45e57231
andrbor <andrbor02@yandex.ru>
2020-11-15 12:51:38 +0300

    1.8.4

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,11 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] a = new int[n];
-        double sum = 0;
-        boolean flag = true;
-        int count = 0;
-
-        for(int i = 0; i < n; i++)
-            a[i] = in.nextInt();
+        int numb = 4;
 
         for(int y = 0; y < n; y++) {
-            if (a[y] % 2 != 0) {
-                sum += a[y];
-                flag = false;
-                count++;
-            }
+            a[y] = numb + 3 * y;
+            System.out.printf(a[y] + " ");
         }
-        if(flag)
-            System.out.println("NO");
-        else
-            System.out.printf("%.2f",sum/count);
     }
 }
