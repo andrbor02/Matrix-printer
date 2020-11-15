710342a38d160bcc2c5aaed161f3ef5a96fca3fb
andrbor <andrbor02@yandex.ru>
2020-11-15 13:49:38 +0300

    1.8.7

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,26 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] a = new int[n];
+        int[] b = new int[n];
+        int t = 0;
 
         for(int y = 0; y < n; y++)
             a[y] = in.nextInt();
 
-        int[] list = new int[n];
-
-        int i, j, count, maxcount, num, len;
-
-        len = 0; maxcount = 1; num = 0;
-        for (i = 0; i < n; i++) {
-            count=0;
-            for (j = i; j < n; j++)
-                if (a[i] == a[j])
-                    count++;
-            if (count == maxcount) {
-                list[len] = i;
-                len++;
-            }
-            if (count > maxcount) {
-                maxcount = count;
-                num = i;
-                len = 1;
-                list[0] = i;
+        for(int x = 0; x < n; x++){
+            if(a[x] < 0) {
+                b[t] = a[x];
+                t++;
             }
         }
-            System.out.print(a[list[0]]);
+        for(int x = 0; x < n; x++){
+            if(a[x] >= 0) {
+                b[t] = a[x];
+                t++;
+            }
+        }
+        for(int y = 0; y < n; y++)
+            System.out.print(b[y] + " ");
+
     }
 }
