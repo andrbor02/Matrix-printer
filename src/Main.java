00222265a4ac6c47e6cd06197d5b7b2c7c96313e
andrbor <andrbor02@yandex.ru>
2020-11-16 14:04:46 +0300

    add II 1.8.4

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,15 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
         int[] a = new int[N];
+        int count = 0;
 
         for(int i = 0; i < N; i++)
             a[i] = in.nextInt();
 
         for(int i = 0; i < N; i += 1) {
-            if (a[i] % 2 == 0)
-                System.out.print(a[i] + " ");
+            if (a[i] > 0)
+                count++;
         }
+        System.out.print(count);
     }
 }
