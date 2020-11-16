cca0b9d34e4a8ec8a336ffae879a290c8e9c917f
andrbor <andrbor02@yandex.ru>
2020-11-16 13:57:53 +0300

    add II 1.8.2

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-        int A = in.nextInt();
-        int B = in.nextInt();
-        int C = in.nextInt();
-        int D = in.nextInt();
         int[] a = new int[N];
 
         for(int i = 0; i < N; i++)
-            a[i] = i + 1;
+            a[i] = in.nextInt();
 
-        int k = B;
-
-        for (int i = A - 1; i < B - 1; i++) {
-            for (int j = A - 1; j < k - 1; j++) {
-                int b = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = b;
-            }
-            k--;
-        }
-
-        int g = D;
-
-        for (int i = C - 1; i < D - 1; i++) {
-            for (int j = C - 1; j < g - 1; j++) {
-                int b = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = b;
-            }
-            g--;
-        }
-
-        for(int i = 0; i < N; i++)
+        for(int i = 0; i < N; i += 2)
             System.out.print(a[i] + " ");
     }
 }
