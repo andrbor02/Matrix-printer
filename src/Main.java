b0e3d192b77aafe31882662cf4dceff7e2c08d7d
andrbor <andrbor02@yandex.ru>
2020-11-16 11:38:27 +0300

    add I 1.8.2

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        int[] a = new int[n];
 
-        while(n >= 1){
-            System.out.print(n % 2);
-            n = (n - (n % 2)) / 2;
+        for(int i = 0; i < n - 1; i++)
+            a[i] = in.nextInt();
+
+        for(int x = 0; x < n - 1; x++){
+            for(int y = 0; y < n - 2; y++){
+                if(a[y + 1] < a[y]){
+                    int b = a[y];
+                    a[y] = a[y + 1];
+                    a[y + 1] = b;
+                }
+
+            }
+        }
+
+        for(int t = 0; t < n; t++){
+            if(a[t] != t + 1) {
+                System.out.print(t + 1);
+                break;
+            }
         }
     }
 }
