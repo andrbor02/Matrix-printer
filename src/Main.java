53f51c59ca4dadcf1d33f07cf74cd0a02ceb4ebe
andrbor <andrbor02@yandex.ru>
2020-11-16 11:54:32 +0300

    add I 1.8.4

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int[][] a = new int[n][n];
-        int[][] b = new int[n][n];
-        int t = 1;
+        int ans = 1;
 
-        for(int x = 0; x < n; x++){
-            for(int y = 0; y < n; y++){
-                a[x][y] = t;
-                b[y][x] = t;
-                t++;
-            }
-        }
+        for(int i = 1; i <= n; i++)
+            ans *= i;
 
-//        for(int x = 0; x < n; x++){
-//            for(int y = 0; y < n; y++){
-//                System.out.printf("%3d",a[x][y]);
-//            }
-//            System.out.print("\n");
-//        }
-//        for(int x = 0; x < n; x++){
-//            for(int y = 0; y < n; y++){
-//                System.out.printf("%3d",b[x][y]);
-//            }
-//            System.out.print("\n");
-//        }
-
-        for(int x = 0; x < n; x++){
-            for(int y = 0; y < n; y++){
-                if(a[x][y] == b[x][y])
-                    System.out.print(a[x][y] + " ");
-            }
-        }
-
+        System.out.println(ans);
     }
 }
