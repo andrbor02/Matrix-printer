1a9605867a09c79562532dc7bdc50b8e6867c13a
andrbor <andrbor02@yandex.ru>
2020-11-16 15:10:03 +0300

    add II 1.8.7

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,15 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
         int[] a = new int[N];
+        int count = 1;
 
         for(int i = 0; i < N; i++)
             a[i] = in.nextInt();
 
-        int Max = a[0];
-
-        for(int i = 1; i < N; i += 1) {
-            if (a[i] > Max)
-                Max = a[i];
+        for(int i = 0; i < N - 1; i++) {
+            if (a[i] != a[i + 1])
+                count++;
         }
-        System.out.print(Max);
+        System.out.print(count);
     }
 }
