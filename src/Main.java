3875f1d6432739068a48eed4a90ef19b643d4c7c
andrbor <andrbor02@yandex.ru>
2020-11-17 22:29:44 +0300

    1.11.1

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int A = in.nextInt();
-        int B = in.nextInt();
-        int C = in.nextInt();
-        int D = in.nextInt();
+        int N = in.nextInt();
+        int M = in.nextInt();
 
-        System.out.println(min(A, B, C, D));
-    }
+        int[][] a = new int[N][M];
+        int[][] b = new int[M][N];
 
-    static int min (int a, int b, int c, int d) {
-        int Min = a;
-        if(b < Min)
-            Min = b;
-        if(c < Min)
-            Min = c;
-        if(d < Min)
-            Min = d;
-        return Min;
+        for(int x = 0; x < N; x++){
+            for(int y = 0; y < M; y++){
+                a[x][y] = in.nextInt();
+            }
+        }
+
+        int copy_N;
+        for(int x = 0; x < M; x++){
+            copy_N = N;
+            for(int y = 0; y < N; y++){
+                b[x][y] = a[copy_N - 1][x];
+                if(copy_N > 1)
+                    copy_N--;
+            }
+        }
+        
+        System.out.println(M + " " + N);
+        for(int x = 0; x < M; x++){
+            for(int y = 0; y < N; y++){
+                System.out.print(b[x][y] + " ");
+            }
+            System.out.print("\n");
+        }
     }
 }
