959c124f30b1e1181057e36a24be7f2487e20b1a
andrbor <andrbor02@yandex.ru>
2020-11-17 21:33:54 +0300

    add 1.9.1

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int[] a = new int[N];
+        int ax = in.nextInt();
+        int ay = in.nextInt();
+        int bx = in.nextInt();
+        int by = in.nextInt();
+        int cx = in.nextInt();
+        int cy = in.nextInt();
 
-        for(int i = 0; i < N; i++)
-            a[i] = in.nextInt();
-
-        f(a, N);
+        System.out.printf("%.10f", P(ax, ay, bx, by, cx, cy));
     }
 
-    public static void f(int[] mass, int numb) {
-        int count = 0;
-        double sum = 0;
+    static double P (int ax, int ay, int bx, int by, int cx, int cy) {
+        double d1, d2, d3;
 
-        for(int p = 0; p < numb; p++) {
-            if((int)Math.log10(mass[p]) == 3){
-                sum += mass[p];
-                count++;
-            }
-        }
-        if(sum == 0)
-            System.out.println("-1.00");
-        else {
-            double ans = sum / count;
-            System.out.printf("%.2f", ans);
-        }
+        d1 = Math.sqrt(Math.pow(ax - bx, 2) + Math.pow(ay - by, 2));
+        d2 = Math.sqrt(Math.pow(bx - cx, 2) + Math.pow(by - cy, 2));
+        d3 = Math.sqrt(Math.pow(cx - ax, 2) + Math.pow(cy - ay, 2));
+
+        double P = d1 + d2 + d3;
+        return P;
     }
 }
