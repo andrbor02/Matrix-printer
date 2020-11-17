0b827eaf77a9c62de95bb7137acc30337df2ac78
andrbor <andrbor02@yandex.ru>
2020-11-17 13:05:34 +0300

    1.9.2

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
 
-        System.out.println(sum_of_digits(n));
+        System.out.println(sum_of_digits());
     }
 
-    public static int sum_of_digits(int a) {
+    public static int sum_of_digits() {
         int sum = 0, numb = 0;
 
-        for (int i = 0; i < Math.log10(a) + 1; i++) {
-            numb = (int) ((a % (int) (Math.pow(10, (i + 1))) - numb) / Math.pow(10, i));
-            sum += numb;
+        for(int p = 14; p < 100; p+= 7) {
+            for (int i = 0; i < Math.log10(p) + 1; i++) {
+                numb = (int) ((p % (int) (Math.pow(10, (i + 1))) - numb) / Math.pow(10, i));
+                sum += numb;
+            }
         }
         return sum;
     }
